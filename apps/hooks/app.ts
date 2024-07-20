29840736985bafdd380cf4f3057091b201684190
Masood ul Rehman <masooddulrehman@gmail.com>
2024-07-20 21:17:58 +0500

    completed first api, updated schema

--- apps/hooks/app.ts
+++ apps/hooks/app.ts
@@ -1,12 +1,39 @@
-
 import { Hono } from "hono";
+import { z } from "zod";
+import { PrismaClient } from "@prisma/client";
+import { zValidator } from "@hono/zod-validator";
 const app = new Hono();
 
+const client = new PrismaClient();
 app.get("/", (c) => {
   return c.text("Hello, world!");
 });
 
-app.get("/hello/:name", (c) => {
-  return c.text(`Hello, ${c.req.param("name")}!`);
-}); 
+app.post(
+  "/hooks/catch/:userId/:zapId",
+  zValidator(
+    "param",
+    z.object({
+      userId: z.string(),
+      zapId: z.string(),
+    })
+  ),
+  async (c) => {
+    const { userId, zapId } = c.req.param();
+    await client.$transaction(async (tx) => {
+      const zap = await tx.zapRun.create({
+        data: {
+          zapId: zapId,
+        },
+      });
+      await tx.zapRunOutbox.create({
+        data: {
+          zapRunId: zap.id,
+        },
+      });
+    });
+
+    return c.json("zap");
+  }
+);
 app.fire();
